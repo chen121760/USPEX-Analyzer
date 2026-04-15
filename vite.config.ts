42f3be21a3aafdca99240a496ebb16074366bea4
Jiawei Chen <2285450581@qq.com>
2026-04-15 21:09:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       },
     },
   ],
-  base: '/uspex-analyzer/',
+  base: '/USPEX-Analyzer/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
